2b86b5728fb5a61acbe9bff09ac5bf519c9d7c38
c4dhaniel <143543742+c4dhaniel@users.noreply.github.com>
2025-04-27 00:39:54 -0500

    tags

--- js/productos.js
+++ js/productos.js
@@ -1,11 +1,19 @@
 // Datos de productos (simulando una base de datos)
 const products = [
-    {id: 1, name: "Hamburguesa", price: 8.50, category: "comidas", image: "https://placehold.co/300x200"},
-    {id: 2, name: "Pizza", price: 12.00, category: "comidas", image: "https://placehold.co/300x200"},
-    {id: 3, name: "Ensalada", price: 6.50, category: "comidas", image: "https://placehold.co/300x200"},
-    {id: 4, name: "Refresco", price: 2.00, category: "bebidas", image: "https://placehold.co/300x200"},
-    {id: 5, name: "Agua Mineral", price: 1.50, category: "bebidas", image: "https://placehold.co/300x200"},
-    {id: 6, name: "Jugo Natural", price: 3.00, category: "bebidas", image: "https://placehold.co/300x200"},
-    {id: 7, name: "Tarta de Chocolate", price: 4.50, category: "postres", image: "https://placehold.co/300x200"},
-    {id: 8, name: "Helado", price: 3.50, category: "postres", image: "https://placehold.co/300x200"}
+    {id: 1, name: "Hamburguesa", price: 8.50, category: "comidas", image: "https://placehold.co/300x200",
+    tags: ["Mayonesa", "Ají", "Mostaza", "Aceituna", "Ketchu"]},
+    {id: 2, name: "Pizza", price: 12.00, category: "comidas", image: "https://placehold.co/300x200",
+    tags: ["Mayonesa", "Ají", "Mostaza", "Aceituna", "Ketchu"]},
+    {id: 3, name: "Ensalada", price: 6.50, category: "comidas", image: "https://placehold.co/300x200",
+    tags: ["Mayonesa", "Ají", "Mostaza", "Aceituna", "Ketchu"]},
+    {id: 4, name: "Refresco", price: 2.00, category: "bebidas", image: "https://placehold.co/300x200",
+    tags: ["Mayonesa", "Ají", "Mostaza", "Aceituna", "Ketchu"]},
+    {id: 5, name: "Agua Mineral", price: 1.50, category: "bebidas", image: "https://placehold.co/300x200",
+    tags: ["Mayonesa", "Ají", "Mostaza", "Aceituna", "Ketchu"]},
+    {id: 6, name: "Jugo Natural", price: 3.00, category: "bebidas", image: "https://placehold.co/300x200",
+    tags: ["Mayonesa", "Ají", "Mostaza", "Aceituna", "Ketchu"]},
+    {id: 7, name: "Tarta de Chocolate", price: 4.50, category: "postres", image: "https://placehold.co/300x200",
+    tags: ["Mayonesa", "Ají", "Mostaza", "Aceituna", "Ketchu"]},
+    {id: 8, name: "Helado", price: 3.50, category: "postres", image: "https://placehold.co/300x200",
+    tags: ["Mayonesa", "Ají", "Mostaza", "Aceituna", "Ketchu"]}
 ];
